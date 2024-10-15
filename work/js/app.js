430c0aa553e3fd89cfc1be149bd5f68990458999
majimajimazzy <j320252@g.kogakuin.jp>
2024-10-15 21:58:14 +0900

    完成

--- work/js/app.js
+++ work/js/app.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('work/data/product.json')
+  fetch('data/product.json')
   .then(response => response.json())
   .then(products => {
     const productList = document.getElementById('productTable');
+    const productItem = document.createElement('tr');
     for (let i = 0; i < products.length; i++){
-      const product = users[i];
-      const productItem = document.createElement('td');
-      productItem.textContent = `ID: ${product.id}, 商品名: ${product.name},価格: ${product.price}, 説明: ${product.description}`;
+      const product= products[i];
+      productList.appendChild(productItem);
+      const productItemStart = document.createElement('th');
+      productTable.innerHTML += `<td>${product.id}</td><td>${product.name}</td><td>${product.price}</td><td>${product.description}</td>`
     }
   })
   .catch(error => console.error('エラー:', error));
